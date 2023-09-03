60ebcf21190cc12d563d12597a018a07fc6a360c
Mayank Kashyap <triplemk7@gmail.com>
2023-09-03 19:06:47 +0530

    initial

---
+++ .config/i3/polybar.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+#echo "---" | tee -a /tmp/polybar1.log 
+polybar top 2>&1 | tee -a /tmp/polybar1.log 
+
+#if type "xrandr"; then
+#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#    MONITOR=$m polybar --reload top &
+#  done
+#else
+#  polybar --reload top &
+#fi
