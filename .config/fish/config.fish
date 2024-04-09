a5cb17b26a76c947e39ba8b6612cd7788ddd5b42
Mayank Kashyap <triplemk7@gmail.com>
2024-04-09 15:28:53 +0530

    stuff
    tmux doesnt seem to work + dont need it
    show i3 windows via rofi
    i need kitty paste
    rofi

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,8 +4,8 @@ set fish_greeting
 starship init fish | source
 
 if status is-interactive
-and not set -q TMUX
-    exec tmux
+# and not set -q TMUX
+#     exec tmux
 end
 
 function vol
@@ -15,6 +15,6 @@ end
 function xbl
   xbacklight -set $argv
 end
-
+ 
 # Created by `pipx` on 2024-03-22 19:44:24
 set PATH $PATH /home/mayank/.local/bin
