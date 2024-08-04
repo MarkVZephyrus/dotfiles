0428f9f5d9b2340c14343913cdc0d38a1ef7faa6
Mayank Kashyap <triplemk7@gmail.com>
2024-08-04 13:02:25 +0200

    rename and general stuff, also multi-monitor.

--- dot-config/polybar/launch.sh
+++ dot-config/polybar/launch.sh
@@ -8,12 +8,12 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
 #echo "---" | tee -a /tmp/polybar1.log 
-polybar top 2>&1 | tee -a /tmp/polybar1.log 
+#polybar top 2>&1 | tee -a /tmp/polybar1.log 
 
-#if type "xrandr"; then
-#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#    MONITOR=$m polybar --reload top &
-#  done
-#else
-#  polybar --reload top &
-#fi
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top &
+  done
+else
+  polybar --reload top &
+fi
