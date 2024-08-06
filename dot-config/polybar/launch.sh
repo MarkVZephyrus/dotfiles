dd26f495574809cc0eac9b3d9bad2bfc74810ab5
Mayank Kashyap <triplemk7@gmail.com>
2024-08-07 01:39:46 +0200

    Lotta shit
    Mainly:
    polybar stuff
    tmux
    general polish

--- dot-config/polybar/launch.sh
+++ dot-config/polybar/launch.sh
@@ -12,7 +12,7 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload top &
+    MONITOR=$m polybar --reload top & 
   done
 else
   polybar --reload top &
