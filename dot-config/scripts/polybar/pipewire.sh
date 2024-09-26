4664732d23724fe18d1fba30f7a4a11821496e63
Mayank Kashyap <triplemk7@gmail.com>
2024-09-26 07:34:21 +0200

    slighlty better volume output
    the icon changes to "Buds" when sink connected via bluetooth

--- dot-config/scripts/polybar/pipewire.sh
+++ dot-config/scripts/polybar/pipewire.sh
@@ -1,31 +1,35 @@
 #!/bin/sh
 
 getDefaultSink() {
-    defaultSink=$(pactl info | awk -F : '/Default Sink:/{print $2}')
-    description=$(pactl list sinks | sed -n "/${defaultSink}/,/Description/s/^\s*Description: \(.*\)/\1/p")
-    echo "${description}"
+  defaultSink=$(pactl info | awk -F : '/Default Sink:/{print $2}')
+  description=$(pactl list sinks | sed -n "/${defaultSink}/,/Description/s/^\s*Description: \(.*\)/\1/p")
+  echo "${description}"
 }
 
 getDefaultSource() {
-    defaultSource=$(pactl info | awk -F : '/Default Source:/{print $2}')
-    description=$(pactl list sources | sed -n "/${defaultSource}/,/Description/s/^\s*Description: \(.*\)/\1/p")
-    echo "${description}"
+  defaultSource=$(pactl info | awk -F : '/Default Source:/{print $2}')
+  description=$(pactl list sources | sed -n "/${defaultSource}/,/Description/s/^\s*Description: \(.*\)/\1/p")
+  echo "${description}"
 }
 
 VOLUME=$(pamixer --get-volume-human)
 SINK=$(getDefaultSink)
 SOURCE=$(getDefaultSource)
-
+ICON="󰕾"
+if [[ "$(pactl info | awk -F : '/Default Source:/{print $2}')" =~ "bluez" ]]; then
+  ICON="󱡒"
+fi
 case $1 in
-    "--up")
-        pamixer --increase 1
-        ;;
-    "--down")
-        pamixer --decrease 1
-        ;;
-    "--mute")
-        pamixer --toggle-mute
-        ;;
-    *)
-        echo "󰕾 ${VOLUME}"
+"--up")
+  pamixer --increase 1
+  ;;
+"--down")
+  pamixer --decrease 1
+  ;;
+"--mute")
+  pamixer --toggle-mute
+  ;;
+*)
+  echo "$ICON ${VOLUME}"
+  ;;
 esac
