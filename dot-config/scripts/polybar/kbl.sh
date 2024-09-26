b9a763f689db20f56395cd95b336cb8289192ab8
Mayank Kashyap <triplemk7@gmail.com>
2024-09-26 07:05:47 +0200

    keyboard layout switcher + interval tweaks

---
+++ dot-config/scripts/polybar/kbl.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Keyboard layout
+
+LANG=("de" "us")
+CURRENT="$(setxkbmap -query | grep layout | awk '{ print $2 }')"
+ICON=󰧺
+IT=0
+while [[ $IT -ne ${#LANG[@]} ]]; do
+  if [[ "$CURRENT" = "${LANG[$IT]}" ]]; then
+    break
+  fi
+  let IT++
+done
+if [[ "$1" = "--switch" ]]; then
+  if [[ $IT -eq $((${#LANG[@]} - 1)) ]]; then
+    ((IT = 0))
+  else
+    ((IT++))
+  fi
+  setxkbmap ${LANG[$IT]}
+else
+  echo "$ICON $CURRENT"
+fi
