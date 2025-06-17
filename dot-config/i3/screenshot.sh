43b36277f484140517ea9dc1a0a2ed8a0cecf229
Mayank Kashyap <triplemk7@gmail.com>
2025-06-17 09:26:50 +0200

    feature update baby
    read code for details lmao

--- dot-config/i3/screenshot.sh
+++ dot-config/i3/screenshot.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 # Use the current timestamp as the unique filename of the screenshot.
-FILE_PATH="/home/$USER/Pictures/Screenshots/ss_$(date -u +'%Y%m%d-%H%M%SZ').png"
-
+FILE_PATH1="/home/$USER/Pictures/Screenshots/ss_$(date -u +'%Y%m%d-%H%M%S').png"
+TMP_FILE_PATH="/var/tmp/ss_$(date -u +'%Y%m%d-%H%M%S').png"
 main() {
   case $1 in
-  full) maim --format=png $FILE_PATH ;;
-  select) maim --select $FILE_PATH ;;
-  window) maim --window $(xdotool getactivewindow) $FILE_PATH ;;
-  screen) maim -x :0.0 $FILE_PATH ;;
+  full) maim --format=png $TMP_FILE_PATH ;;
+  select) maim --select $TMP_FILE_PATH ;;
+  window) maim --window $(xdotool getactivewindow) $TMP_FILE_PATH ;;
+  screen) maim -x :0.0 $TMP_FILE_PATH ;;
   esac
 }
-
 main "$@"
-notify-send "Screenschot saved." "Screenshot saved at ~/Pictures/Screenshots." --app-name="Maim" -i $FILE_PATH
+cat $TMP_FILE_PATH | xclip -selection clipboard -target image/png -i
+notify-send "Screenschot saved." "Screenshot saved to clipboard" --app-name="Maim" -i $TMP_FILE_PATH
